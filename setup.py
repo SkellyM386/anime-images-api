fbec407c952d24e9f94e35c2cc2d3674ecd90947
Skelly Mclane <74203498+SkellyM386@users.noreply.github.com>
2021-05-06 08:27:50 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="anime-image-api",
+    version='0.0.0',
+    author="Skelly386 (skelly386#1410)",
+    author_email="skellymclane@gmail.com",
+    description="",
+    long_description_content_type="text/markdown",
+    long_description="",
+    packages=find_packages(),
+    install_requires=['download', 'rich', 'requests', 'os', 'json'],
+    keywords=['python'],
+    classifiers=[]
+)
